bf4c3f75e8fdbdbd15f58c9f6631e8951a599c07
Patrick Dougall <pdougall1@gmail.com>
2015-11-02 22:29:01 -0500

    Adds active scope to projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,6 @@
 class Project < ActiveRecord::Base
   has_many :project_user_roles
   belongs_to :company
+
+  scope :active, -> { where(active: true) }
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -4,4 +4,11 @@ describe Project, :type => :model do
   subject { FactoryGirl.create(:project) }
   it { is_expected.to respond_to :company }
   it { is_expected.to respond_to :project_user_roles }
+
+  describe '.active' do
+    let!(:active_project)   { FactoryGirl.create(:project, active: true) }
+    let!(:inactive_project) { FactoryGirl.create(:project, active: false) }
+    subject { described_class.active }
+    it { is_expected.to match_array [active_project] }
+  end
 end
